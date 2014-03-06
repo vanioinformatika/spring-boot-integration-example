b9f5e42b8b9ccd63db426b62d2ba8d034ac7dc94
Gyula Szalai <gyula.szalai@vanio.hu>
2014-03-06 12:41:48 +0100

    First full implementation

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
-spring-boot-integration-example
-===============================
+Spring-integration project botstrapped with Spring-boot
+=======================================================
 
-SpringBoot integration example 
+SpringBoot integration example with an MTOM enabled webservice gateway.
+
+To try out:
+-----------
+
+1. check out the project from GitHub
+1. install JDK 1.7+ (Java7)
+1. install Maven 3.0.3+
+1. set JAVA_HOME environment variable to home of the installed JDK 1.7
+1. go to the project directory and issue mvn clean spring-boot:run
+1. open a browser and enter the following address: http://localhost:8080/contentStore.wsdl
+1. use the WSDL to create a client of your preference
+
---
+++ src/main/java/hu/vanio/spring/boot/integration/ExampleIntegrationApplication.java
@@ -0,0 +1,47 @@
+package hu.vanio.spring.boot.integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
+import org.springframework.ws.wsdl.WsdlDefinition;
+import org.springframework.ws.wsdl.wsdl11.SimpleWsdl11Definition;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class ExampleIntegrationApplication {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(ExampleIntegrationApplication.class, args);
+    }
+    
+    @Bean
+    public ServletRegistrationBean dispatcherServlet() {
+        MessageDispatcherServlet dispatcherServlet = new MessageDispatcherServlet();
+        dispatcherServlet.setTransformWsdlLocations(true);
+        ServletRegistrationBean servletDef = new ServletRegistrationBean(dispatcherServlet, "/contentStore", "*.wsdl");
+        servletDef.addInitParameter("contextConfigLocation", "classpath:integration-context.xml");
+        servletDef.setLoadOnStartup(1);
+        return servletDef;
+    }
+    
+    @Bean
+    public WsdlDefinition contentStore() {
+        return new SimpleWsdl11Definition(new ClassPathResource("/contentStore.wsdl"));
+    }
+    
+    @Bean
+    public Object marshaller() {
+        Jaxb2Marshaller marshaller = new Jaxb2Marshaller(); 
+        marshaller.setMtomEnabled(true);
+        marshaller.setContextPath("hu.vanio.springwsmtom.wstypes");
+        return marshaller;
+    }
+
+}
