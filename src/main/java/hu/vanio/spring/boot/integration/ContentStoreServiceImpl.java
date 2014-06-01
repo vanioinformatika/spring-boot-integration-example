db68eeb37d15c6bbc1f4e5695637286d4a59b619
Gyula Szalai <gyula.szalai@vanio.hu>
2014-06-01 22:25:01 +0200

    Enabled WS-Security on the WS endpoint

--- src/main/java/hu/vanio/spring/boot/integration/ContentStoreServiceImpl.java
+++ src/main/java/hu/vanio/spring/boot/integration/ContentStoreServiceImpl.java
@@ -10,7 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.integration.MessagingException;
+import org.springframework.messaging.MessagingException;
 import org.springframework.stereotype.Component;
 
 import hu.vanio.springwsmtom.wstypes.LoadContentRequest;
--- src/main/java/hu/vanio/spring/boot/integration/ExampleIntegrationApplication.java
+++ src/main/java/hu/vanio/spring/boot/integration/ExampleIntegrationApplication.java
@@ -6,8 +6,12 @@ import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
 import org.springframework.ws.wsdl.WsdlDefinition;
 import org.springframework.ws.wsdl.wsdl11.SimpleWsdl11Definition;
@@ -22,7 +26,7 @@ public class ExampleIntegrationApplication {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(ExampleIntegrationApplication.class, args);
     }
-    
+
     @Bean
     public ServletRegistrationBean wsDispatcherServlet() {
         MessageDispatcherServlet wsDispatcherServlet = new MessageDispatcherServlet();
@@ -32,25 +36,42 @@ public class ExampleIntegrationApplication {
         servletDef.setLoadOnStartup(1);
         return servletDef;
     }
-    
+
     @Bean
     public WsdlDefinition contentStore() {
         return new SimpleWsdl11Definition(new ClassPathResource("/contentStore.wsdl"));
     }
-    
+
     @Bean
     public XsdSchema contentStoreSchema() {
         SimpleXsdSchema xsdSchema = new SimpleXsdSchema();
         xsdSchema.setXsd(new ClassPathResource("contentStoreSchema.xsd"));
         return xsdSchema;
     }
-    
+
     @Bean
     public Object marshaller() {
-        Jaxb2Marshaller marshaller = new Jaxb2Marshaller(); 
+        Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
         marshaller.setMtomEnabled(true);
         marshaller.setContextPath("hu.vanio.springwsmtom.wstypes");
         return marshaller;
+    }
+
+    @Bean
+    public ApplicationSecurity applicationSecurity() {
+        return new ApplicationSecurity();
+    }
+            
+    @Order(Ordered.LOWEST_PRECEDENCE - 8)
+    public static class ApplicationSecurity extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/contentStore", "*.wsdl").permitAll();
+        }
     }
 
 }
